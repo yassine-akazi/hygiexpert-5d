7db9996995bb2ec80e25c67ad723727d7788d661
Yassine Akazi <96472822+yassine-akazi@users.noreply.github.com>
2025-05-26 05:54:12 +0100

    Add test-db route to check DB connection

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    uild: {
-        outDir: 'dist',  // Change le dossier de sortie en 'dist'
-        // si besoin, autres configs ici
-      },
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  build: {
+    outDir: 'dist',  // <- ça va générer la build dans dist/
+  },
 });
